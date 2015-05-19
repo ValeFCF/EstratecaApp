e57b9d4a792399f9591b7e2866cf148946957c68
Valentin Granados <valefcfhc@gmail.com>
2015-05-19 01:32:49 -0500

    request webservice

--- app/src/main/java/estrateca/com/app/fragments/Cuenta.java
+++ app/src/main/java/estrateca/com/app/fragments/Cuenta.java
@@ -5,6 +5,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import estrateca.com.app.R;
 
@@ -31,8 +40,46 @@ public class Cuenta extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_cuenta, container, false);
+
+        final TextView mTextView = (TextView) v.findViewById(R.id.text);
+        final EditText mEditText = (EditText) v.findViewById(R.id.editText);
+        final Button mButton = (Button) v.findViewById(R.id.button);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(getActivity()); //getActivity() -> this
+                String url = "http://www.payport.esmifavorito.com/api/service/checkService/";
+
+                // Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url + mEditText.getText().toString(),
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
+                                mTextView.setText("Response is: " + response); // reponse.substring(0, 500)
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        mTextView.setText("That didn't work!");
+                    }
+                });
+                // Add the request to the RequestQueue.
+                queue.add(stringRequest);
+
+
+            }
+        });
+
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cuenta, container, false);
+        return v;
     }
 
 }
